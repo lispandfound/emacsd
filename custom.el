1a02305cef873a3d39d5f48dcac2fbfc3c54a9da
Jake Faulkner <jakefaulkn@gmail.com>
2022-09-27 08:53:41 +1300

    Custom Emacs changes

--- custom.el
+++ custom.el
@@ -3,22 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("f0eb51d80f73b247eb03ab216f94e9f86177863fb7e48b44aacaddbfe3357cf1" default))
  '(package-selected-packages
-   '(visual-regexp-steroids hyperbole flymake-collection pdf-tools smudge citar slime gap maxima exec-path-from-shell eglot marginalia popwin dumb-jump gap-mode reftex apheleia lsp-mode posframe yasnippet auctex cdlatex magit which-key avy tree-sitter-langs tree-sitter orderless consult corfu general undo-tree vertico use-package nano-theme nano-modeline evil))
- '(smtpmail-smtp-server "localhost")
- '(smtpmail-smtp-service 1025)
- '(widget-image-enable nil)
- '(x-underline-at-descent-line t))
-
+   '(meow eglot popwin gap-mode tree-sitter-langs tree-sitter auctex embark flymake-collection corfu which-key magit cdlatex use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(nano-italic ((t (:family "Victor Mono" :slant italic :height 110 :weight regular))))
- '(nano-mono ((t (:weight light :height 110 :family "RobotoMono Nerd Font"))))
- '(nano-mono-alt ((t (:weight light :height 110 :family "Fira Code"))))
- '(nano-sans ((t (:weight light :height 110 :family "Roboto"))))
- '(nano-serif ((t (:weight light :height 110 :family "Roboto Slab")))))
+ )
