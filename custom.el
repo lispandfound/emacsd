95c55a02aedaeff5c9fa3a55da9797d6b3857c50
Jake Faulkner <jakefaulkn@gmail.com>
2022-09-27 14:51:25 +1300

    embark, orderless, vertico, etc

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(meow eglot popwin gap-mode tree-sitter-langs tree-sitter auctex embark flymake-collection corfu which-key magit cdlatex use-package)))
+   '(embark-consult marginalia consult orderless vertico meow eglot popwin gap-mode tree-sitter-langs tree-sitter auctex embark flymake-collection corfu which-key magit cdlatex use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
